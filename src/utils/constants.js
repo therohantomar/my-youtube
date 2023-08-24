99813b2ff5027cb48ce0c1e007743b930d010ab0
therohantomar <therohantomar@gmail.com>
2023-08-24 14:42:37 +0530

    changing Youtube search api link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,7 +18,7 @@ export const Buttonlistdata = [
   "Tourism",
 ];
 export const YOUTUBE_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=`;
-export const YOUTUBE_AUTOCOMPLETE_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_AUTOCOMPLETE_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 export const YOUTUBE_SEARCH_API= `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=${process.env.REACT_APP_APIKEY}&order=relevance&q=`
 
 export const CommentsData=[
