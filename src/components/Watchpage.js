bd7cc95ba9451bfad476c83af10d290ecaf3a653
therohantomar <therohantomar@gmail.com>
2023-10-14 21:47:03 +0530

    added livecomment block

--- src/components/Watchpage.js
+++ src/components/Watchpage.js
@@ -11,16 +11,17 @@ const Watchpage = () => {
   const video = useSelector((store) => store?.video?.videoItems);
   const [searchParams] = useSearchParams();
   const id = searchParams.get("v");
-  useScrollToTop()
+  useScrollToTop();
 
-  if(!id) return <h1>loading.....</h1>
+  if (!id) return <h1>loading.....</h1>;
 
   return (
+    <>
     <div className=" flex xs:flex-col sm:flex-col overflow-y-scroll scrollbar-hide -moz-scrollbar-none   md:flex-col lg:flex-col xl:flex-row 2xl:flex-row  gap-2 h-max   w-full   ">
       <div className=" flex flex-col  gap-4 h-max  overflow-y-scroll scrollbar-hide -moz-scrollbar-none  pb-32 w-full p-2 border-r-2   ">
         <span className="xl:w-[58rem] my-2 lg:w-[38rem] md:w-[32rem] sm:w-[24rem] xs:h-[12rem] sm:h-[16rem] md:h-[20rem] lg:h-[22rem] xl:h-[32rem] ">
           <iframe
-           className="w-full h-full"
+            className="w-full h-full"
             src={`https://www.youtube.com/embed/${id}`}
             title="YouTube video player"
             frameBorder="0"
@@ -31,7 +32,9 @@ const Watchpage = () => {
           ></iframe>
         </span>
         <p className="text-lg font-bold ">{video?.title}</p>
-        <p className="text-gray-400">{video?numify(video?.viewCount):null} views</p>
+        <p className="text-gray-400">
+          {video ? numify(video?.viewCount) : null} views
+        </p>
         {/* channel info  */}
         <span className="flex items-center py-2 border-y-2  gap-4">
           <BiUserCircle className="w-12 text-gray-300 h-10" />
@@ -43,10 +46,12 @@ const Watchpage = () => {
         <CommentsContainer />
       </div>
       {/* videos rendered  */}
-      <span className="w-96">
-        <VideosContainer />
+      <span className="w-full h-full">
+        <div className="border-2 border-black rounded-lg h-[400px]"></div>
       </span>
     </div>
+    
+      </>
   );
 };
 
